62a44bf5be5bed14a55446e760c077ded601e4e0
AnkitKyada-devloper <119589163+AnkitKyada-devloper@users.noreply.github.com>
2023-02-10 18:57:26 +0530

    fix

--- resources/views/mail_link.blade.php
+++ resources/views/mail_link.blade.php
@@ -14,7 +14,7 @@
         <p>Click the button below to log in to MOE Leave app.</p>
         <p>This button will expire in 5 minutes</p>
         <a style="text-decoration: none;" href= "{{$maildetails['body'] }}"><h2 style="background: #00466a;margin: 0 auto;width: max-content;padding: 0 10px;color: #fff;border-radius: 4px;">Login to App</h2></a>
-        <p style="font-size:15px;">Regards,<br />Team openeyes</p>
+        <p style="font-size:15px;">Regards,<br />Team openeyes </p>
         <hr style="border:none;border-top:5px solid #eee" />
         <div style="float:right;padding:8px 0;color:#aaa;font-size:0.8em;line-height:1;font-weight:300">
  </div>
